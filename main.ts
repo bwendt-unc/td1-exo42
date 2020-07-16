34d7e0f3c7749c69ea4d3933276871100e931a24
bwendt-unc <68039945+bwendt-unc@users.noreply.github.com>
2020-07-16 13:51:10 +1100

    Fichiers initiaux pour un projet MakeCode

---
+++ main.ts
@@ -0,0 +1,38 @@
+input.onButtonPressed(Button.A, function () {
+    if (true) {
+        led.unplot(0, 0)
+        y += 1
+    } else if (false) {
+        led.unplot(0, 0)
+        y = 0
+        y = 0
+    } else if (false) {
+        led.unplot(0, 0)
+        y += 1
+        y += 1
+    } else {
+    	
+    }
+})
+input.onButtonPressed(Button.B, function () {
+    if (true) {
+        led.unplot(0, 0)
+        y += 1
+    } else if (false) {
+        led.unplot(0, 0)
+        y = 0
+        y = 0
+    } else if (false) {
+        led.unplot(0, 0)
+        y += 1
+    } else {
+    	
+    }
+})
+let y = 0
+let x = 2
+y = 0
+led.plot(x, y)
+basic.forever(function () {
+	
+})
